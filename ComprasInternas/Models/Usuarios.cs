87ebc28af2ffaf3d01080c18fefa29e07d3d1728
jav0314 <javiermerida134@gmail.com>
2025-04-15 22:25:48 -0500

    Modelos

---
+++ ComprasInternas/Models/Usuarios.cs
@@ -0,0 +1,11 @@
+﻿namespace ComprasInternas.Models
+{
+    public class Usuarios
+    {
+        public int IdUsuario { get; set; }
+        public string? NombreUsuario { get; set; }
+        public int IdRol { get; set; }
+        public int Estado { get; set; }
+
+    }
+}
