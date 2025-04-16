cbad6717f3fa6b54b02c90acce310379f7871dd3
jav0314 <javiermerida134@gmail.com>
2025-04-16 10:25:49 -0500

    permitir conexion con front angular

--- ComprasInternas/Program.cs
+++ ComprasInternas/Program.cs
@@ -8,7 +8,17 @@ builder.Services.AddScoped<SolicitudSupervisorDAO>();
 builder.Services.AddScoped<RegisterDAO>();
 builder.Services.AddScoped<LoginDAO>();
 
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:63603") // frontend Angular
+            .WithOrigins("http://localhost:4200") // frontend Angular
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -17,7 +27,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseCors("AllowAngularApp");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
